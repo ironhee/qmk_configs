b160ce8f51afd68c0c90aaabf6dc85cf4f3245df
이철희 <leechulhee95@gmail.com>
2024-06-13 20:34:34 +0900

    ploopy classic 키맵 살짝 변경

--- keymaps/ploopy_classic/config.h
+++ keymaps/ploopy_classic/config.h
@@ -2,11 +2,11 @@
 
 #define PLOOPY_DPI_OPTIONS \
     {                      \
-        1200, 1600         \
+        800, 1200, 1600    \
     }
 #define PLOOPY_DPI_DEFAULT 1
 #define PLOOPY_DRAGSCROLL_FIXED 1
-#define PLOOPY_DRAGSCROLL_DPI 100
+#define PLOOPY_DRAGSCROLL_DPI 50
 #define PLOOPY_DRAGSCROLL_INVERT 1
 
 #define COMBO_COUNT 1
--- keymaps/ploopy_classic/keymap.c
+++ keymaps/ploopy_classic/keymap.c
@@ -4,7 +4,8 @@
  * Layer
  * https://github.com/qmk/qmk_firmware/blob/master/docs/feature_layers.md
  */
-enum layers {
+enum layers
+{
     DEFT, // Default
     FUNC, // Function
 };
@@ -13,16 +14,16 @@ enum layers {
  * Combo keys
  * https://github.com/qmk/qmk_firmware/blob/master/docs/feature_combo.md
  */
-const uint16_t PROGMEM btn1_btn3_combo[] = {KC_BTN1, KC_BTN2, COMBO_END};
+const uint16_t PROGMEM btn1_btn3_combo[] = {KC_BTN1, KC_BTN3, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
-    COMBO(btn1_btn3_combo, KC_BTN3),
+    COMBO(btn1_btn3_combo, DRAG_SCROLL),
 };
 
 /**
  * Keymaps
  */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [DEFT] = LAYOUT(KC_BTN1, KC_BTN3, KC_BTN2, OSL(FUNC), DRAG_SCROLL),
+    [DEFT] = LAYOUT(KC_BTN1, KC_BTN3, KC_BTN2, KC_BTN3, MO(FUNC)),
     [FUNC] = LAYOUT(KC_BTN4, QK_BOOT, KC_BTN5, DPI_CONFIG, _______),
 };
